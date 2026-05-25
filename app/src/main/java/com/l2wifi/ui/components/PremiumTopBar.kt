44b7ad4c4d6f9a64653dcf346f67475665608b5c
leonar2if <Leonar20081007@gmail.com>
2026-05-25 14:47:19 -0400

    Actualizar PremiumTopBar.kt

--- app/src/main/java/com/l2wifi/ui/components/PremiumTopBar.kt
+++ app/src/main/java/com/l2wifi/ui/components/PremiumTopBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PremiumTopBar(
     title: String,
